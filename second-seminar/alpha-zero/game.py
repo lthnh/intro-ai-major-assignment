749e717c97aae6c49134f3eeed7c226700bae8d9
Thomas <imr3ng4l@gmail.com>
2023-11-30 01:08:45 +0700

    implementing the game to match other's representation

--- second-seminar/alpha-zero/game.py
+++ second-seminar/alpha-zero/game.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import numpy as np
 import numpy.typing as npt
 
@@ -6,20 +8,33 @@ class UltimateTicTacToe():
     def get_initial_board(self):
         # 0: canonical form
         # 1: turn (1 for X and 0 for O)
-        # 2: won sub boards
-        return np.zeros((3, 9, 9))
+        # 2: last move
+        # 3: won sub boards
+        return np.zeros((4, 9, 9))
     def get_next_state(self, board: npt.NDArray, action: tuple):
-        board[action[0]][action[1]] = board[1][0]
+        board[0][action[0]][action[1]] = 1 if board[1][0] else -1
         return board
-    def get_valid_moves(self, board: npt.NDArray) -> list[tuple[int, int]]:
-        return list(zip(*np.where(board == 0)))
+    def get_valid_moves(self, board: npt.NDArray) -> list[Optional[tuple[int, int]]]:
+        last_move: tuple[int, int] = (idx.item() for idx in board[2].nonzero())
+        empty_position_global: list[tuple[int, int]] = list(zip(*np.where(board == 0)))
+        legal_moves = []
+        for r, c in self.__sub_board_indices:
+            if last_move[0] >= r and last_move[1] >= c:
+                relative_row = last_move[0] - r
+                relative_column = last_move[1] - c
+                relative_idx = 3*relative_row + relative_column
+                sub_board_next_row, sub_board_next_column = self.__sub_board_indices[relative_idx]
+                sub_board_next = board[sub_board_next_row:sub_board_next_row+3][sub_board_next_column:sub_board_next_column+3]
+                empty_position_local = np.argwhere(sub_board_next == 0)
+                legal_moves = empty_position_local if len(empty_position_local) > 0 else empty_position_global
+                return legal_moves
     def get_game_ended(self, board: npt.NDArray):
+        
         pass
-    def _check_sub_board(self, board):
+    def _check_result_sub_boards(self, board):
         for r, c in self.__sub_board_indices:
             sub_board = board[r:r+3][c:c+3]
             column_sum = sub_board.sum(axis=0)
             row_sum = sub_board.sum(axis=1)
-            neg_diag_sum = sub_board.trace()
-            pos_diag_sum = np.rot90(sub_board).trace()
-            
+            negative_diagonal_sum = sub_board.trace()
+            positive_diagonal_sum = np.rot90(sub_board).trace()
